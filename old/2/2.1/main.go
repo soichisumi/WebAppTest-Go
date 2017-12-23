99e55e888205b4636ce428d5d93cd5a8be23443a
yoyoyousei <yoyoyousei2016@gmail.com>
2017-12-23 15:30:36 +0900

    2.1実装

---
+++ old/2/2.1/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+	"html/template"
+	"path/filepath"
+	"log"
+	"flag"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+	r := newRoom()
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/room", r)
+	go r.run()
+	log.Println("Webサーバを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
