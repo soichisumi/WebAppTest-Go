ff18fea9bd75dd4af223e6009787c57622af4739
yoyoyousei <yoyoyousei2016@gmail.com>
2017-12-24 17:23:35 +0900

    3.1を追加

---
+++ old/3/3.1/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"time"
+)
+
+type client struct {
+	socket   *websocket.Conn
+	send     chan *message
+	room     *room
+	userData map[string]interface{}
+}
+
+func (c *client) read(){
+	for{ //無限ループ
+		var msg *message
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
+			if avatarURL, ok := c.userData["avatar_url"]; ok {
+				msg.AvatarURL = avatarURL.(string)
+			}
+			c.room.forward<-msg
+		}else{
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write(){
+	for msg := range c.send{
+		if err := c.socket.WriteJSON(msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
